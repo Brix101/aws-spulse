f2f4012d167960acfc9d0da23f42bd00b26c5827
Brixter Porras <brixterporras@gmail.com>
2024-05-18 16:23:14 +0800

    feat: added initial app setup with trpc

---
+++ web/src/provider/query-provider.tsx
@@ -0,0 +1,32 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "../utils/trpc";
+import { queryClient } from "../lib/react-query";
+import { httpBatchLink } from "@trpc/client";
+import React from "react";
+
+export function QueryProvider({ children }: React.PropsWithChildren) {
+  const [trpcClient] = React.useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "/trpc",
+          // You can pass any HTTP headers you wish here
+          // async headers() {
+          //   return {
+          //     authorization: getAuthCookie(),
+          //   };
+          // },
+        }),
+      ],
+    }),
+  );
+  return (
+    <>
+      <trpc.Provider client={trpcClient} queryClient={queryClient}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </trpc.Provider>
+    </>
+  );
+}
