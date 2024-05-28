f8f83540628e070da4a2a5118730c2b7685756c6
Brixter Porras <brixterprras@gmail.com>
2024-05-28 16:33:41 +0800

    feat: added signal traps exit process

--- api/src/app.ts
+++ api/src/app.ts
@@ -35,12 +35,30 @@ export const startServer = async () => {
     console.log(`server started on http://localhost:${env.PORT}/trpc`);
   });
 
-  process.on("SIGTERM", () => {
-    console.log("SIGTERM signal received.");
-    console.log("Closing http server.");
-    server.close((err) => {
-      console.log("Http server closed.");
-      process.exit(err ? 1 : 0);
+  //////////////////////////////////////////////////////////////////////
+  const signals = ["SIGTERM", "SIGINT", "SIGHUP", "SIGBREAK"];
+  const errorTypes = ["unhandledRejection", "uncaughtException"];
+
+  errorTypes.forEach((type) => {
+    process.on(type, async (error) => {
+      try {
+        console.error(`process exit due to ${type}`);
+        console.error(error);
+        process.exit(1);
+      } catch (_) {
+        process.exit(1);
+      }
+    });
+  });
+
+  signals.forEach((type) => {
+    process.on(type, () => {
+      console.log(`${type} signal received.`);
+      console.log("Closing http server.");
+      server.close((err) => {
+        console.log("Http server closed.");
+        process.exit(err ? 1 : 0);
+      });
     });
   });
 };
